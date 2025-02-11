115512a36bf81ce65921006c31542e84a2eb12c7
Adam Hlavacek <git@adamhlavacek.com>
2025-02-11 22:34:44 +0100

    feat(apier): support also static pages

--- src/ci_cd_apier/apier.py
+++ src/ci_cd_apier/apier.py
@@ -1,12 +1,24 @@
 import json
+from enum import Enum
 from uuid import UUID
 from pathlib import Path
 from typing import Dict, Callable, Optional
 from os import getcwd, environ
 from traceback import print_exc
 from datetime import datetime, timezone
+from shutil import copytree
 
 from ssage import SSAGE
+import jinja2
+from jinja2.sandbox import SandboxedEnvironment
+
+from .patcher import patch_html
+
+
+class APIEREndpointMode(Enum):
+    API = "api"
+    TEMPLATE = "template"
+    RAW = "raw"
 
 
 class APIERClientError(Exception):
@@ -31,33 +43,66 @@ class APIER:
     APIER class to have Flask-like routing for CI/CD requests
     """
 
-    def __init__(self, age_key: str, dir_responses: Optional[Path] = None):
+    def __init__(self,
+                 age_key: str,
+                 dir_webpage: Optional[Path] = None,
+                 dir_responses: Optional[Path] = None,
+                 dir_templates: Optional[Path] = None,
+                 dir_static: Optional[Path] = None):
         """
         Initialize the APIER object
         :param age_key: local secret key
-        :param dir_responses: directory to store responses
+        :param dir_webpage: directory to store all webpages
+        :param dir_responses: directory to store responses, defaults to responses in webpage or the current directory
+        :param dir_templates: directory to store templates, defaults to templates in the current directory
+        :param dir_static: directory to store static files, defaults to static in the current directory
         """
         self.__decryptor = SSAGE(age_key)
-        self.__dir_responses = dir_responses or (Path(getcwd()) / "responses")
-        self.__paths: Dict[str, Callable[[any], str]] = {}
+        self.__dir_responses = dir_responses
+        self.__dir_webpage = dir_webpage or Path(getcwd()) / "public"
+        if not self.__dir_responses:
+            self.__dir_responses = self.__dir_webpage / "responses"
+        self.__dir_templates = dir_templates or Path(getcwd()) / "templates"
+        self.__dir_static = dir_static or Path(getcwd()) / "static"
+        self.__paths: Dict[APIEREndpointMode, Dict[str, Callable[[any], str]]] = {
+            APIEREndpointMode.API: {},
+            APIEREndpointMode.TEMPLATE: {},
+            APIEREndpointMode.RAW: {}
+        }
 
-    def register_path(self, path: str, handler: Callable[[any], str]) -> None:
+    def render_template(self, template_name: str, **kwargs) -> str:
+        """
+        Render a template with the given arguments
+        :param template_name: name of the template file
+        :param kwargs: arguments to pass to the template
+        :return: rendered template
+        """
+        if not self.__dir_templates:
+            raise FileNotFoundError("Templates directory not found")
+        template_loader = jinja2.FileSystemLoader(searchpath=[str(self.__dir_templates)])
+        template_env = SandboxedEnvironment(loader=template_loader)
+        template = template_env.get_template(template_name)
+        return template.render(**kwargs)
+
+    def register_path(self, path: str, handler: Callable[[any], str], mode: APIEREndpointMode = APIEREndpointMode.API) -> None:
         """
         Register a path with a handler
         :param path: virtual request path
         :param handler: function to handle the request
+        :param mode: endpoint mode, required to decide how to handle the request
         :return: None
         """
-        self.__paths[path] = handler
+        self.__paths[mode][path] = handler
 
-    def route(self, path: str):
+    def route(self, path: str, mode: APIEREndpointMode = APIEREndpointMode.API):
         """
         Decorator to register a path with a handler
         :param path: virtual request path
+        :param mode: endpoint mode, required to decide how to handle the request
         :return: decorator
         """
         def decorator(func: Callable[[any], str]):
-            self.register_path(path, func)
+            self.register_path(path, func, mode)
             return func
         return decorator
 
@@ -66,7 +111,8 @@ class APIER:
             data_env_name: str = "APIER_DATA",
             empty_ok: bool = True,
             always_success: bool = True,
-            delete_old_responses: bool = True
+            delete_old_responses: bool = True,
+            build_static_pages: bool = True
     ) -> None:
         """
         Process the current request stored in the environment variable
@@ -74,12 +120,16 @@ class APIER:
         :param empty_ok: if True, do not raise an error if there is no request
         :param always_success: if True, do not raise an error if there is an exception
         :param delete_old_responses: if True, delete old responses
+        :param build_static_pages: if True, build static pages
         :return: None
         """
         # noinspection PyBroadException
         try:
             if delete_old_responses:
                 self.purge_old_responses()
+            if build_static_pages:
+                self.build_static_pages()
+            self.__dir_responses.mkdir(parents=True, exist_ok=True)
             data = environ.get(data_env_name)
             if not data:
                 if empty_ok:
@@ -92,6 +142,36 @@ class APIER:
                 print_exc()
             else:
                 raise
+
+    def build_static_pages(self) -> bool:
+        """
+        Build static pages from templates
+        :return: True if all pages were built successfully
+        """
+        if self.__dir_webpage is None:
+            return False
+        self.__dir_webpage.mkdir(parents=True, exist_ok=True)
+
+        if self.__dir_static is not None and self.__dir_static.exists():
+            copytree(self.__dir_static, self.__dir_webpage / "static", dirs_exist_ok=True)
+
+        for endpoint_mode in (APIEREndpointMode.RAW, APIEREndpointMode.TEMPLATE):
+            for route_path, function in self.__paths[endpoint_mode].items():
+                response = function(None)
+                if route_path == '/':
+                    route_path = "index.html"
+                else:
+                    route_path = route_path.lstrip('/')
+
+                if endpoint_mode == APIEREndpointMode.TEMPLATE and route_path.count('.') == 0:
+                    route_path = f"{route_path}.html"
+
+                path_response = self.__dir_webpage / route_path
+                path_response.write_text(response)
+                if route_path.lower().endswith('.html'):
+                    patch_html(path_response)
+
+        return True
 
     def process_single_request(self, request_raw: str) -> None:
         """
@@ -126,7 +206,7 @@ class APIER:
 
         print(f'[*] Processing request {request_id}')
 
-        request_handler = self.__paths.get(request_path)
+        request_handler = self.__paths[APIEREndpointMode.API].get(request_path)
         if request_path is None:
             raise APIERClientError(f"Path not registered: {request_path}")
 
